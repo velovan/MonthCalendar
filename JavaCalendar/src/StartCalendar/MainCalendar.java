3ecf723eeb8eea6be814dfd74088dd62d5cf5130
velovan <velovan@users.noreply.github.com>
2022-05-24 13:10:46 +0300

    Update MainCalendar.java

--- JavaCalendar/src/StartCalendar/MainCalendar.java
+++ JavaCalendar/src/StartCalendar/MainCalendar.java
@@ -9,7 +9,7 @@ import SelectedMonth.SelectedMonthMain;
 
 public class MainCalendar {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Calendar today = Calendar.getInstance();
 
 		// Print current month
@@ -25,4 +25,5 @@ public class MainCalendar {
 		SelectedMonthMain clen = new SelectedMonthMain();
 		clen.introducingMonth();
 	}
+	
 }
