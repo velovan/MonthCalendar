e8d19d56c72ce8a51c4f2349251b6852d94e8789
velovan <velovan@users.noreply.github.com>
2022-05-26 12:23:17 +0300

    Update PrintCalendar.java

--- JavaCalendar/src/SelectedMonth/PrintCalendar.java
+++ JavaCalendar/src/SelectedMonth/PrintCalendar.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 
 public class PrintCalendar {
 
-	public static void printCalendar(int numberOfMonthDays, int firstWeekdayOfMonth) {
+	public static void printCalendar(int numberOfMonthDays, int firstWeekdayOfMonth){
 
 		// reset index of weekday
 		int weekdayIndex = 0;
@@ -13,32 +13,28 @@ public class PrintCalendar {
 		// print calendar weekday header
 		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
 
-
 		// leave/skip weekdays before the first day of month
-		for (int day = 1; day < firstWeekdayOfMonth; day++) {
+		for (int day = 1; day < firstWeekdayOfMonth; day++){
 			System.out.print("    ");
 			weekdayIndex++;
 		}
 
 		// print the days of month in tabular format.
-		for (int day = 1; day <= numberOfMonthDays; day++) {
+		for (int day = 1; day <= numberOfMonthDays; day++){
 			// print day
- 
 			System.out.printf("%1$2d", day);
 
 			// next weekday
 			weekdayIndex++;
 			
 			// if it is the last weekday
-			if (weekdayIndex == 7) {
-				 
+			if (weekdayIndex == 7){			
 				// reset it
-				weekdayIndex = 0;
-				
+				weekdayIndex = 0;				
 				// and go to next line
 				System.out.println();
 			} else { // otherwise
-						// print space
+				// print space
 				System.out.print("  ");
 			}
 		}
