f467b8abb610a8f8d32a67faeafcfdd1be6f1eeb
velovan <velovan@users.noreply.github.com>
2022-05-27 16:36:26 +0300

    Update CurrentDate.java

--- JavaCalendar/src/CurrentMonths/CurrentDate.java
+++ JavaCalendar/src/CurrentMonths/CurrentDate.java
@@ -3,21 +3,20 @@ package CurrentMonths;
 import java.time.LocalDate;
 import java.util.Calendar;
 
-public class CurrentDate {
+public class CurrentDate{
   /*
    *This part of program print data for the current month
    * This part of the program consists of three classes
-   */
-	
-	public CurrentDate() {
-		System.out.println("\n ***" + " Calendar for the month" + "***\n");
+   */	
+	public CurrentDate(){
+	   System.out.println("\n ***" + " Calendar for the month" + "***\n");
 	}
 
-	public void currentDate(Calendar cal) {
+	public void currentDate(Calendar cal){
         // Print current date
-		LocalDate today = LocalDate.now();
-		cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
-		System.out.println("Date: " + today.getDayOfMonth() + "| " + today.getDayOfWeek() +  "| " + today.getMonth() + "| " + today.getYear());
+	   LocalDate today = LocalDate.now();
+	   cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+	   System.out.println("Date: " + today.getDayOfMonth() + "| " + today.getDayOfWeek() +  "| " + today.getMonth() + "| " + today.getYear());
 	}
 
 }
