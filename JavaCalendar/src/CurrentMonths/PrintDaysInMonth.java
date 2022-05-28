073b62dd13987a093a489bdadb2c908e2cb941b9
velovan <velovan@users.noreply.github.com>
2022-05-28 21:05:44 +0300

    Update PrintDaysInMonth.java

--- JavaCalendar/src/CurrentMonths/PrintDaysInMonth.java
+++ JavaCalendar/src/CurrentMonths/PrintDaysInMonth.java
@@ -11,11 +11,10 @@ public class PrintDaysInMonth {
 
 	}
 
-	public void showDaysInMonths(Calendar cal) {
+	public void showDaysInMonths(Calendar cal){
 		int month = cal.get(Calendar.MONTH);
 		int firstDayOfWeek = cal.getFirstDayOfWeek();
 		Calendar calendar = Calendar.getInstance();
-
 
 		// Display dates in mont
 		cal.set(Calendar.DATE, cal.getMinimum(Calendar.DATE));
@@ -23,21 +22,17 @@ public class PrintDaysInMonth {
 		// Now display the dates, one week per line
 		StringBuilder week = new StringBuilder();
 
-		while (month == cal.get(Calendar.MONTH)) {
-			
+		while (month == cal.get(Calendar.MONTH)){
 			// Display date
 			week.append(String.format("%3d ", cal.get(Calendar.DATE)));
-			
 			// Increment date
 			cal.add(Calendar.DATE, 1);
-			
 			// Check if week needs to be printed
-			if (cal.get(Calendar.MONTH) != month) {
-				
+			if (cal.get(Calendar.MONTH) != month){
 				// end of month
 				// just need to output the month
 				System.out.println(week);
-			} else if (cal.get(Calendar.DAY_OF_WEEK) == firstDayOfWeek) {
+			} else if (cal.get(Calendar.DAY_OF_WEEK) == firstDayOfWeek){
 				
 				// new week so print out the current week
 				// first check if any padding needed
