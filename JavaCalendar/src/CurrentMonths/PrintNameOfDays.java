891b359c56c7b0de72206aaaccee7bd026637847
velovan <velovan@users.noreply.github.com>
2022-05-29 15:59:29 +0300

    Update PrintNameOfDays.java

--- JavaCalendar/src/CurrentMonths/PrintNameOfDays.java
+++ JavaCalendar/src/CurrentMonths/PrintNameOfDays.java
@@ -6,28 +6,24 @@ import java.time.Month;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class PrintNameOfDays {
+public class PrintNameOfDays{
 
 	// Get the current date
 	LocalDate today = LocalDate.now();
 
-	public PrintNameOfDays() {
+	public PrintNameOfDays(){
 
 	}
 
-	public void showNameOfDays(Calendar cal) {
+	public void showNameOfDays(Calendar cal){
 		int month = cal.get(Calendar.MONTH);
 		int firstDayOfWeek = cal.getFirstDayOfWeek();
 		Calendar calendar = Calendar.getInstance();
-
 		// Display day names as headers
 		System.out.println("\n");
 		System.out.println("      " + "**" + today.getMonth() + "**" + " : " + today.getDayOfMonth());
-
 		System.out.println("___________________________\n ");
-
 		for (int i = 0; i < 7; i++) {
-
 		     System.out.print(cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault().UK));
 		     System.out.print(" ");
 		     cal.add(Calendar.DATE, 1);
